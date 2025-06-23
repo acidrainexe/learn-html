ba635c833d575fd4190dc233080b1337c65c42f3
Naeem Islam <85617842+acidrainexe@users.noreply.github.com>
2025-06-24 03:14:07 +0600

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,7 @@ ytd-rich-grid-renderer-contents, ytd-two-column-browse-results-renderer {
   display: none !important;
 }
 
-/* Optional: Hide other elements (e.g., suggested videos or ads) */
-//ytd-compact-video-renderer, ytd-promoted-sparkles-text-search-renderer {
-//  display: none !important;
-//}
-
-/* Hide the sidebar on the left */
-//ytd-mini-guide-renderer {
-//  display: none !important;
-//}
+/* Optional: Hide other elements (e.g., suggested videos or ads)
+ytd-compact-video-renderer, ytd-promoted-sparkles-text-search-renderer {
+  display: none !important;
+} */
